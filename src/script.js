eb903ed0bd0ae4eebe86c8de621e21d6bff596bd
tangjinlog <lkg70007@gmail.com>
2022-06-23 23:50:46 +0900

    스크롤섹션 배열 설정

--- src/script.js
+++ src/script.js
@@ -1,5 +1,40 @@
 (()=> {
 
+  const sceneInfo = [
+    { 
+      type: 'sticky',
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector('#scroll-section-0'),
+      } 
+    },
+    { 
+      type: 'sticky',
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector('#scroll-section-1'),
+      } 
+    },
+    { 
+      type: 'sticky',
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector('#scroll-section-2'),
+      } 
+    },
+    {
+      type: 'sticky',
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector('#scroll-section-3'),
+      } 
+    },
+  ]
+
   const progressBar = document.querySelector('.progress-bar' ); 
   window.addEventListener('scroll', function() {
     console.log(window.pageYOffset / document.body.offsetHeight)
